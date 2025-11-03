66336868f24089fcfdf375e70ed66a4dfd4b822f
iia tronics <iia.yatskova@gmail.com>
2025-11-03 22:24:40 +0200

    working levenshtein distance counter

--- levenshtein.cpp
+++ levenshtein.cpp
@@ -1,9 +1,21 @@
 #include "header.h"
 
-// int put_in_matrix(int l1, int l2, int dif)
-// {
+int trio_min(int a, int b, int c)
+{
+    if(a <= b && a <= c)
+    {
+        return a;
+    }
+    else if(b <= a && b <= c)
+    {
+        return b;
+    }
+    else
+    {
+        return c;
+    }
+}
 
-// }
 int equal(char a, char b)
 {
     if(a == b)
@@ -51,25 +63,41 @@ int levenshtein_index(string word1, string word2)
 
     // }
 
-    vector<vector<char>> matrix;
+    vector<vector<int>> matrix(word1_lenght+1, vector<int> (word2_lenght+1));
 
-    for (int i = 0; i <= word1_lenght; i++)
+    for (int j = 0; j <= word1_lenght; j++)
     {
-        for (int j = 0; j <= word2_lenght; j++)
-        {
-            matrix[0][i] = i;
-            matrix[j][0] = j;
-        }
+        matrix[j][0] = j;
+    }  
+
+    for (int i = 0; i <= word2_lenght; i++)
+    {
+        matrix[0][i] = i;
     }
 
-    for (int i = 1; i <= word1_lenght; i++)
+    int temp = 0, a = 0, b = 0, c = 0;
+    for (int j = 1; j <= word2_lenght; j++)
     {
-        for (int j = 1; j <= word2_lenght; j++)
+        for (int i = 1; i <= word1_lenght; i++)
         {
             //matrix[i][j] = put_in_matrix(matrix)
-            matrix[i][j] = min({(matrix[i][j - 1] + 1), (matrix[i - 1][j] + 1), matrix[i - 1][j - 1] + equal(word1[i], word2[j])});
-            index = matrix[i][j];
-            cout << "index: " << index << "\n";
+            //matrix[i][j] = min({((int)(matrix[i][j - 1]) + 1), ((int)(matrix[i - 1][j]) + 1), ((int)(matrix[i - 1][j - 1]) + (int)equal(word2[i - 1], word1[j - 1]))});
+            // temp = (matrix[i - 1][j - 1]) + equal(word2[i - 1], word1[j - 1]);
+            // matrix[i][j] = trio_min(((int)(matrix[i][j - 1]) + 1), ((int)matrix[i - 1][j] + 1), temp);
+            // cout << "min of : " << (int)(matrix[i][j - 1]) + 1 << " : " << (int)(matrix[i - 1][j]) + 1 << " : " << (int)(matrix[i - 1][j - 1]) << " + " << equal(word1[i - 1], word2[j - 1]) << endl;
+            // index = matrix[i][j];
+            // cout << "index (" << i << "; " << j << ") :" << index << "\n";
+
+            a = (int)matrix[i][j - 1] + 1;
+            //a = a + 1;
+            b = (int)matrix[i - 1][j] + 1;
+            //b = b + 1;
+            c = (int)matrix[i - 1][j - 1] + equal(word1[i - 1], word2[j - 1]);
+
+            cout << "min of : "<< a <<" | "<<b<< " | " << c << " | word1[i]: "<< word1[i - 1] << " | word2[j]: "<< word2[j - 1]  << endl;
+            index = trio_min(a, b, c);
+            cout << "index (" << i << "; " << j << ") :" << index << "\n";
+            matrix[i][j] = index;
         }
     }
 
@@ -82,7 +110,7 @@ void levenshtein_realisation(string word1, string word2)
 
     //cout << word1 << "  " << word2 << "\n";
     index = levenshtein_index(word1, word2);
-    cout << "index: " << index << "\n";
+    cout << "final index: " << index << "\n";
 
     return;
 }
